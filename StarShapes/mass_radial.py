d2c70701ea0df3434e9b7fb1b1ed84c7c0c3d747
Marko Urbanc <urbancmarko1@gmail.com>
2023-11-22 21:29:59 +0100

    fix: :ambulance: obliterate glaring issue with iteration over P

--- StarShapes/mass_radial.py
+++ StarShapes/mass_radial.py
@@ -20,13 +20,15 @@ def v_tangential(x, y, z):
     r = np.sqrt(x**2 + y**2 + z**2)
     return V_ROT * r / R_MAX
 
-def condition(pairs):
+def condition(pairs, P=P_DEF):
     results = []
     for pair in pairs:
         x, y, z = pair
         check = np.sqrt(np.abs(x)) + np.sqrt(np.abs(y)) + np.sqrt(np.abs(z)) <= 1
         if check:
-            integrand = 1 * np.sqrt(x**2 + y**2 + z**2)**P_DEF
+            integrand = 1 * np.sqrt(x**2 + y**2 + z**2)**P
+        else:
+            integrand = 0
         results.append(integrand)
     return np.array(results)
 
@@ -38,11 +40,13 @@ if True:
     t_exec_dispersion = []
     result_dispersion = []
     integral_dispersion = []
-    for i, N in enumerate(P_range):
-        nn = NumberNecromancer(condition, num_samples=N, num_dimensions=3, domain=[-1, 1])
+    for i, P in enumerate(P_range):
+        cond = lambda x: condition(x, P=P)
+        nn = NumberNecromancer(cond, num_samples=N, num_dimensions=3, domain=[-1, 1])
         if nn.rank == 0:
             print(f"Running {i + 1} of {len(P_range)}")
         n_in, n_tot, t_exec, res = nn.run()
+        print
         N_dispersion.append(n_tot)
         result_dispersion.append(n_in)
         integral_dispersion.append(res)
