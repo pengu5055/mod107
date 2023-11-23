04547234028fc7f4b0b17ea5c24b22b49188ebc1
Marko Urbanc <urbancmarko1@gmail.com>
2023-11-23 11:22:44 +0000

    feat: :package: add results for pt. 2 and 3

--- queue.sh
+++ queue.sh
@@ -7,6 +7,6 @@
 # and will be run with the command:
 # ./queue.sh
 
-mpiexec -np 152 --hostfile hosts python3 -m mpi4py ./GammaBirth/unit_sphere.py && \
-mpiexec -np 152 --hostfile hosts python3 -m mpi4py ./NeutronReflector/linear_motion.py && \
+mpiexec -np 152 --hostfile hosts python3 -m mpi4py ./GammaBirth/unit_sphere.py; \
+mpiexec -np 152 --hostfile hosts python3 -m mpi4py ./NeutronReflector/linear_motion.py; \
 mpiexec -np 152 --hostfile hosts python3 -m mpi4py ./NeutronReflector/isotropic_motion.py
